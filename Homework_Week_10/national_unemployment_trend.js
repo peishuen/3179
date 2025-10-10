09edc745ff246432d7443dbea08e453b9347a3d8
oooi0001 <oooi0001@student.monash.edu>
2025-10-10 13:22:09 +0800

    Homework Week 10

---
+++ Homework_Week_10/national_unemployment_trend.js
@@ -0,0 +1,3 @@
+var vg = "Homework_Week_10/national_unemployment_trend.vg.json";
+
+vegaEmbed("#nationalUnemploymentTrend", vg).then(function(result){}).catch(console.error);
--- Homework_Week_9/unemploymentMap.js
+++ Homework_Week_9/unemploymentMap.js
@@ -1,3 +1,3 @@
 var vg = "Homework_Week_9/unemploymentMap.vg.json";
 
-vegaEmbed("#unemploymentMap", vg).then(function(result){}).catach(console.error);
+vegaEmbed("#unemploymentMap", vg).then(function(result){}).catch(console.error);
